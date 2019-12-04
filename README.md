2b216c85de93b46f3a279a2026423ffd504f48f5
kor0p <viktorua25.03@gmail.com>
2019-12-04 14:10:38 +0200

    add tests and do clean code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# IoTLab5Algorithms
+# IoTLab4Algorithms
+Setup
+--
+`python3 main.py {file_name} {result}`  
+or  
+`py main.py {file_name} {result}`  
+and enter your input values  
+For example:  
+`py main.py input 2`
+
+**python2 not supported!**
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-with open('input') as f:
-    W, H = map(int, f.readline().split())
-    field = tuple(map(''.join, zip(*(f.readline()[:-1] for _ in range(H)))))
-#transpose_field = tuple((f.readline()[:-1] for h in range(H)))
-#field = tuple(map(''.join, zip(*transpose_field)))
+import sys
 
-sumField = [[0]*H for _ in range(W)]
-sumField[-1][0] = sumField[-1][-1] = 1
-for w in range(W-2, -1, -1):
-    for h in range(H):
-        sumField[w][h] = sum((
-            sumField[j][i]
-            for i in range(H)
-            for j in range(w+1, W)
-            if field[w][h] == field[j][i] or (i == h and j == w+1)
-        ))
-print('N: ', sum(sumField[0]))
+def main(file):
+    with open(file) as f:
+        width, height = map(int, f.readline().split())
+        field = tuple(map(''.join, zip(*(f.readline()[:-1] for _ in range(height)))))
+    print(width, height)
+    print(*tuple(map(''.join, zip(*field))), sep='\n')
+
+    sumField = [[0]*height for _ in range(width)]
+    sumField[-1][0] = sumField[-1][-1] = 1
+    for w in range(width-2, -1, -1):
+        for h in range(height):
+            sumField[w][h] = sum((
+                sumField[j][i]
+                for i in range(height)
+                for j in range(w+1, width)
+                if field[w][h] == field[j][i] or (i == h and j == w+1)
+            ))
+    return sum(sumField[0])
+
+def test(file, res):
+    N = main(file)
+    assert N == res
+    print(f'[TEST PASSED] N: {N}')
+
+
+test('inp1', 5)
+test('inp2', 2)
+test('inp3', 201684)
+test('inp4', 14)
+print('ALL TESTS PASSED\nYOUR EXAMPLE:')
+test(sys.argv[1], int(sys.argv[2]))
